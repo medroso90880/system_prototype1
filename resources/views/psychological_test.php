695211cb6bfbcad529b9680978e2984779792c14
jasper <jasperdaquila@gmail.com>
2022-08-16 15:52:23 +0800

    added educ family health psycho components

---
+++ resources/views/psychological_test.php
@@ -0,0 +1,416 @@
+<header class="text-center">
+    <h2 class="text-2xl font-bold uppercase mb-6 box-decoration-slice bg-gradient-to-r from-red-500 to-blue-500 text-white px-2 rounded">
+        PSYCHOLOGICAL TEST PROFILE
+    </h2>
+</header>
+
+<form method="POST" action="/listings" enctype="multipart/form-data">
+
+    <div class="mb-2">
+            <div class="text-center text-lg">
+            <label class="inline-block py-2 pr-3 font-bold" >Mental Ability</label> 
+            </div>
+            <div>
+            <label class="inline-block py-2 pr-16 pl-80 ml-3.5" >Culture Fair</label>
+            <label class="inline-block py-2 pl-28" >OLMAT</label>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-1 ml-1 pl-48 " >Raw Score</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-200 ml-1"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-200 ml-12"/>
+            </div>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-1 ml-60 pl-0.5 " >DIQ</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-200 ml-1"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-200 ml-12"/>
+            </div>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-1 ml-56 pl-4 " >%ile</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-200 ml-1"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-200 ml-12"/>
+            </div>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-1 pl-44 ml-3 " >Description</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-200 ml-1"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-200 ml-12"/>
+            </div>
+        </div>
+
+
+        
+        <div class="text-center text-lg">
+            <label class="inline-block py-2 pr-3 font-bold" >Aptitude</label> 
+            </div>
+            <div>
+            <label class="inline-block py-2 pr-24 ml-11 pl-60" >Raw Score</label>
+            <label class="inline-block py-2 pr-24">Percentile</label>
+            <label class="inline-block py-2" >Description</label>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-8 mr-2.5 pl-44" >VA</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-40"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            </div>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-10 pl-44" >NA</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-40"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            </div>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-2.5 pl-44" >VR+NA</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-40"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            </div>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-8 mr-2.5 pl-44" >AR</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-40"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            </div>
+            
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-7 mr-2.5 pl-44" >MR</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-40"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            </div>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-11 pl-44" >SR</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-40"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            </div>
+            
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-12 mr-1.5  pl-44" >S</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-40"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            </div>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-12 mr-1.5 pl-44" >L</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-40"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-40 ml-3"/>
+            </div>
+        </div>
+
+        <div class="text-center text-lg">
+            <label class="inline-block py-2 font-bold" >Study Habits and Attitudes</label> 
+            </div>
+            <div>
+            <label class="inline-block py-2 pr-10 pl-80 ml-14" >Raw Score</label>
+            <label class="inline-block py-2 pr-16 pl-7">Percentile</label>
+            <label class="inline-block py-2" >Description</label>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-12 ml-44">Delay Avoidance</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-32"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            </div>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-14 mr-1 ml-44" >Work Methods</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-32"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            </div>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-16 mr-3 ml-44" >Study Habits</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-32"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            </div>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-8 mr-2.5 ml-44" >Teacher Approval</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-32"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            </div>
+            
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-2 ml-44" >Education Acceptance</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-32"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            </div>
+
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-14 mr-1.5 ml-44" >Study Attitude</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-32"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            </div>
+            
+            <div class="mb-2">
+            <label class="inline-block py-2 pr-8 mr-1.5 ml-44" >Study Orientation</label> 
+            <input type="text" class="border border-gray-150 rounded p-1 w-32"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            <input type="text" class="border border-gray-150 rounded p-1 w-32 ml-3"/>
+            </div>
+        </div>
+        <div class="py-5 text-center">
+        <label class="my-5 font-bold text-lg">Personality</label>
+        </div>
+            <table class="border-collapse border w-full">
+            <thead>
+    <tr>
+      <th class="border border-slate-600 w-84 ...">Low Score Description</th>
+      <th class="border border-slate-600 text-xs w-52">1 2 3 4 5 6 7 8 9 10</th>
+      <th class="border border-slate-600 ...">High Score Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="border border-slate-700 ...">RESERVED, detached, critical, cool</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">OUTGOING, warmhearted, easy-going, participating</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">LESS INTELEGENT, concrete-thinking</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">MORE INTELEGENT, ABSTRACT-THINKING, bright</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">AFFECTED BY FEELINGS, emotionally less stable, easily upset, changeable</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">EMOTIONALLY STABLE, faces reality, calm</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">PHLEGMATIC, deliberate, inactive, stodgy</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">EXCITABLE, IMPATIENT, DEMANDING, OVERACTIVE</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">OBEDIENT, mild, conforming</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">ASSERTIVE, independent, aggressive, stuborm</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">SOBER, prudent, serious, tacitum</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">HAPPY-GO-LUCKY, headless, gay, enthusiastic</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">DISREGARDS RULES, undependable, by-passes obligations</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">CONCIENTIOUS, preserving, staid, rule-bound</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">SHY, restrained, different, timid</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">VENTURSOME, socially bold, uninhibited, spountaneous</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">TOUGH-MINDED, self-reliant, realistic, no-nonsense</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">TINDER-MINDED, dependent, over-protected, sensitve</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">VIGOROUS, goes readily with group, zestful, given to action</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">DOUBTING, obstructive, individualistic, reflective, internally restrained, unwilling to act</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">SELF-ASSURED, placid, secure, serene</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">APPREHENSIVE, worrying, depressive, resourceful</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">GROUP-DEPENDENT, a "joiner" and sound follower</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">SELF-SUFFICIENT, prefers own decisions, resourceful</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">CASUAL, careless of society rules, untidy, follows own urges</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">CONTROLLED, socially-precise, self-disciplined, compulsive</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-700 ...">RELAXED, tranquil, torpid, understated</td>
+      <td class="text-center border border-slate-700 m-1">
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+        <input type="radio"/>
+      </td>
+      <td class="border border-slate-700 ...">TENSE, driven, overwrought, fretful</td>
+    </tr>
+  </tbody>
+ </table>
+</div>
+
+    <div>
+    <label>Counselor's Notes:</label>
+    <input type="text" class="border border-gray-150 rounded p-1 w-full py-12"/>
+    
+  </div>
+               
+    </div>
+</form>
+</div>
+
+</form>
