c9a7fabda4a453242559791ce44c924f067bd30d
princelloyd <princelloyd.cagampang@gmail.com>
2022-08-16 15:00:17 +0800

    added social_profile and student_self_asses to resources/views

---
+++ resources/views/components/social_profile.blade.php
@@ -0,0 +1,73 @@
+<header class="text-center">
+    <h2 class="text-2xl font-bold uppercase mb-6">
+        SOCIAL PROFILE
+    </h2>
+</header>
+
+<!--Name-->
+<form method="POST" action="/listings" enctype="multipart/form-data">
+    <div>
+        <h4 class="font-bold uppercase mb-6">Do you have real friends:</h4>
+        <div class="px-10 mb-4 flex justify-between">
+            <label class="inline-block mb-2">Of your age?</label>
+            <select class="mr-16" style="background-color: powderblue">
+                <option>Yes</option>
+                <option>No</option>
+            </select>
+        </div>
+        <div class="px-10 mb-4 flex justify-between">
+            <label class="inline-block mb-2">Older than you?</label>
+            <select class="mr-16" style="background-color: powderblue">
+                <option>Yes</option>
+                <option>No</option>
+            </select>
+        </div>
+        <div class="px-10 mb-4 flex justify-between">
+            <label class="inline-block mb-2">Younger than you?</label>
+            <select class="mr-16" style="background-color: powderblue">
+                <option>Yes</option>
+                <option>No</option>
+            </select>
+        </div>
+    </div>
+    <div>
+        <h4 class="font-bold uppercase mb-6 mt-6">Are most of your friends:</h4>
+        <div class="px-10 mb-4 flex justify-between">
+            <label class="inline-block mb-2">Boys?</label>
+            <select class="mr-16" style="background-color: powderblue">
+                <option>Yes</option>
+                <option>No</option>
+            </select>
+        </div>
+        <div class="px-10 mb-4 flex justify-between">
+            <label class="inline-block mb-2">Girls?</label>
+            <select class="mr-16" style="background-color: powderblue">
+                <option>Yes</option>
+                <option>No</option>
+            </select>
+        </div>
+        <div class="px-10 mb-4 flex justify-between">
+            <label class="inline-block mb-2">From another school?</label>
+            <select class="mr-16" style="background-color: powderblue">
+                <option>Yes</option>
+                <option>No</option>
+            </select>
+        </div>
+        <div class="px-10 mb-4 flex justify-between">
+            <label class="inline-block mb-2">From this school?</label>
+            <select class="mr-16" style="background-color: powderblue">
+                <option>Yes</option>
+                <option>No</option>
+            </select>
+        </div>
+    </div>
+    <div>
+        <h4 class="font-bold uppercase mb-6 mt-6">List down your best friends at PSHS:</h4>
+        <input type="Text" class="border border-gray-200 rounded p-2 w-full" name="company"/>
+    </div>
+    <div>
+        <h4 class="font-bold uppercase mb-6 mt-6">List down your hobbies, special talents,
+             skills, interests or leisure time activities:</h4>
+        <input type="Text" class="border border-gray-200 rounded p-2 w-full" name="company"/>
+    </div>
+</form>
---
+++ resources/views/components/student_self_assessment.blade.php
@@ -0,0 +1,279 @@
+<header class="text-center">
+    <h2 class="text-2xl font-bold uppercase mb-10">
+        STUDENT'S SELF-ASSESSMENT
+    </h2>
+</header>
+
+<div>
+    <h6 class="py-6 font-bold">Based on self-observation, feedback gained from other people and
+        personal experiences, honestly assess yourself on the following areas in a scale of 1-5 where;
+        1 - Exellent, 2 - Very Good, 3 - Good, 4 - Fair, 5 - Poor.</h6>
+</div>
+<div class="mb-2 font-bold flex justify-between px-10 pb-5">
+    <div>
+        <h6>Assessment</h6>
+    </div>
+    <div>
+        <h6>1</h6>
+    </div>
+    <div>
+        <h6>2</h6>
+    </div>
+    <div>
+        <h6>3</h6>
+    </div>
+    <div>
+        <h6>4</h6>
+    </div>
+    <div>
+        <h6>5</h6>
+    </div>
+</div>
+<div class="flex justify-between">
+    <div class="-mr-8">
+        <p>PHYSICAL WELL-BEING</p>
+        <p>(appearance, health,</p>
+        <p>grooming)</p>
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-8" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-8" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-8" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-8" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mr-9 mt-8" />
+    </div>
+</div>
+<div class="flex justify-between mt-5">
+    <div class="-mr-11">
+        <p>MENTAL:</p>
+        <p>Comprehension,</p>
+        <p>Reasoning & Judgement, </p>
+        <p>Alertness</p>
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-10" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-10" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-10" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-10" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mr-9 mt-10" />
+    </div>
+</div>
+<div class="flex justify-between mt-5">
+    <div class="-mr-8">
+        <p>SPEECH</p>
+        <p>(fluency, diction,</p>
+        <p>voice, manner of </p>
+        <p>expression, spontaneity</p>
+        <p> of thoughts)</p>
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-14" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-14" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-14" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-14" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mr-9 mt-14" />
+    </div>
+</div>
+<div class="flex justify-between mt-5">
+    <div class="-mr-7">
+        <p>PERSONALITY</p>
+        <p>(self-confidence,</p>
+        <p>social graces, </p>
+        <p>attitude and values,</p>
+        <p>interpersonal skills,</p>
+        <p>emotional adaptability)</p>
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-16" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-16" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-16" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-16" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mr-9 mt-16" />
+    </div>
+</div>
+<div class="flex justify-between mt-5 mb-5">
+    <div class="-mr-5">
+        <p>CHARACTER</p>
+        <p>(integrity, industry</p>
+        <p>& resourcefulness,</p>
+        <p>observance of school</p>
+        <p>rules & regulation,</p>
+        <p>cooperation,</p>
+        <p>responsibility, respect</p>
+        <p>for others & property)</p>
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-20" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-20" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-20" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mt-20" />
+    </div>
+    <div>
+        <input type="checkbox" class="rounded mr-9 mt-20" />
+    </div>
+</div>
+
+<div class="py-4 font-bold">
+    <h6>Select any of the following words which describe your general make-up:</h6>
+</div>
+
+<div>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-28">Perserving</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-36">Calm</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block">Friendly</label>
+</div>
+<div>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-32 mr-2">Patient</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-28 mr-0.5">Stubborn</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block">Capable</label>
+</div>
+<div>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-32">Tolerant</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-28">Impulsive</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block">Pessimistic</label>
+</div>
+<div>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-32 mr-1.5">Jealous</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-36 mr-2.5">Shy</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block">Talented</label>
+</div>
+<div>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-20 mr-1">Self-Confident</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-16">Quick-tempered</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block">Cynical</label>
+</div>
+<div>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-32 mr-3">Tactful</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-24 mr-1.5">Submissive</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block">Conscientious</label>
+</div>
+<div>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-24 mr-2">Poor health</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-28 mr-2.5">Cheerful</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block">Excitable</label>
+</div>
+<div>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-32 mr-0.5">Irritable</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-28 mr-3.5">Anxious</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block">Nervous</label>
+</div>
+<div>
+    <input type="checkbox" class="rounded pl-4" />
+    <label class="inline-block pr-28 mr-2.5">Unhappy</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-28 mr-1.5">Frequent</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block">Daydreaming</label>
+</div>
+<div>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block pr-16 mr-1.5">Easily Exhausted</label>
+    <input type="checkbox" class="rounded" />
+    <label class="inline-block">Frequent periods of gloom or depression</label>
+</div>
+
+<div class="py-6 mt-2 font-bold">
+    <h6>Complete the following statements:</h6>
+</div>
+<div class="flex justify-center mb-3">
+    <label for="statement" class="inline-block">My father and my mother:</label>
+    <input type="text" class="border border-gray-200 rounded w-full"/>
+</div>
+<div class="flex justify-center mb-3">
+    <label for="statement" class="inline-block">My greatest mistake:</label>
+    <input type="text" class="border border-gray-200 rounded w-full"/>
+</div>
+<div class="flex justify-center mb-3">
+    <label for="statement" class="inline-block">I wish my parents were:</label>
+    <input type="text" class="border border-gray-200 rounded w-full"/>
+</div>
+<div class="flex justify-center mb-3">
+    <label for="statement" class="inline-block">In school, my teacher:</label>
+    <input type="text" class="border border-gray-200 rounded w-full"/>
+</div>
+<div class="flex justify-center mb-3">
+    <label for="statement" class="inline-block">I like friends because:</label>
+    <input type="text" class="border border-gray-200 rounded w-full"/>
+</div>
+<div class="flex justify-center mb-3">
+    <label for="statement" class="inline-block">Compared with most families, mine:</label>
+    <input type="text" class="border border-gray-200 rounded w-full"/>
+</div>
+<div class="flex justify-center mb-3">
+    <label for="statement" class="inline-block">I always wanted to:</label>
+    <input type="text" class="border border-gray-200 rounded w-full"/>
+</div>
+<div class="flex justify-center mb-3">
+    <label for="statement" class="inline-block">I feel that my father:</label>
+    <input type="text" class="border border-gray-200 rounded w-full"/>
+</div>
+<div class="flex justify-center mb-3">
+    <label for="statement" class="inline-block">At school, I get along with:</label>
+    <input type="text" class="border border-gray-200 rounded w-full"/>
+</div>
+<div class="flex justify-center mb-3">
+    <label for="statement" class="inline-block">My mother:</label>
+    <input type="text" class="border border-gray-200 rounded w-full"/>
+</div>
