029bdf98fb8d0a599622b52b618976c7b70c9dc8
jemuel <medzjem@gmail.com>
2022-08-17 10:19:11 +0800

    added tables on the database

--- database/migrations/2022_08_10_051803_personal_info.php
+++ database/migrations/2022_08_10_051803_personal_info.php
@@ -52,17 +52,11 @@ return new class extends Migration
             $table->string('contact_number_father');
             $table->string('contact_number_mother');
             $table->string('marital_status');
-            $table->timestamps();
-        });
-
-        //creating table for siblings
-        Schema::create('siblings', function (Blueprint $table){
-            $table->foreignId('personal_info_id');
             $table->string('name_sibling');
-            $table->integer('age');
-            $table->string('educational_attain');
-            $table->string('school_attended');
-            $table->string('occupation');
+            $table->integer('age_sibling');
+            $table->string('educational_attain_sibling');
+            $table->string('school_attended_sibling');
+            $table->string('occupation_sibling');
             $table->timestamps();
         });
 
@@ -108,6 +102,11 @@ return new class extends Migration
             $table->string('honors/awards_received');
             $table->string('sectarian/non-sect');
         //
+            $table->string('grade_level');
+            $table->string('section');
+            $table->string('scholarship_status');
+            $table->string('academic_status');
+        //
             $table->string('subject_like_best');
             $table->string('subject_like_least');
             $table->string('subject_find_difficult');
@@ -115,14 +114,6 @@ return new class extends Migration
             $table->string('subject_learned_least');
             $table->string('subject_taught_best');
             $table->string('subject_taught_worst');
-        });
-        //creating another table for eduaction background
-        Schema::create('educational_bg_bs', function (Blueprint $table){
-            $table->foreignId('personal_info_id');
-            $table->string('grade_level');
-            $table->string('section');
-            $table->string('scholarship_status');
-            $table->string('academic_status');
         });
 
         //creating database for social profile
@@ -134,7 +125,7 @@ return new class extends Migration
             $table->integer('friends_boys');
             $table->integer('friends_girls');
             $table->integer('friend_other_school');
-            $table->integer('from_this_school');
+            $table->integer('friend_this_school');
             $table->longText('list_friends');
             $table->longText('list_talents_hobbies'); 
             $table->timestamps();
@@ -149,6 +140,7 @@ return new class extends Migration
             $table->integer('speech_filipino');
             $table->integer('written_communication_skills');
             $table->integer('personality');
+            $table->integer('character');
             $table->longText('general_make_up');
             $table->string('statement_1');
             $table->string('statement_2');
@@ -162,28 +154,134 @@ return new class extends Migration
             $table->string('statement_10');
             $table->timestamp();  
         });
+
         //creating table for extra curricular activities
         Schema::create('extra_curricular_acts', function (Blueprint $table){
             $table->foreignId('personal_info_id');
-            //add grade foreignkey
-            $table->string('position');
-            $table->string('org/club');
-            $table->string('activity');
-            $table->string('extent_activity');
-            $table->string('nature_evolvement');
+            //postition held in school
+            $table->string('position_g7');
+            $table->string('position_g8');
+            $table->string('position_g9');
+            $table->string('position_g10');
+            $table->string('position_g11');
+            $table->string('position_g12');
+            $table->string('org/club_g7');
+            $table->string('org/club_g8');
+            $table->string('org/club_g9');
+            $table->string('org/club_g10');
+            $table->string('org/club_g11');
+            $table->string('org/club_g12');
+            //involvement in activities
+            $table->string('activity_g7');
+            $table->string('activity_g8');
+            $table->string('activity_g9');
+            $table->string('activity_g10');
+            $table->string('activity_g11');
+            $table->string('activity_g12');
+            $table->string('extent_activity_g7');
+            $table->string('extent_activity_g8');
+            $table->string('extent_activity_g9');
+            $table->string('extent_activity_g10');
+            $table->string('extent_activity_g11');
+            $table->string('extent_activity_g12');
+            $table->string('nature_evolvement_a_g7');
+            $table->string('nature_evolvement_a_g8');
+            $table->string('nature_evolvement_a_g9');
+            $table->string('nature_evolvement_a_g10');
+            $table->string('nature_evolvement_a_g11');
+            $table->string('nature_evolvement_a_g12');
+            //special academic honors awards/distinctions received
+            $table->string('award_g7');
+            $table->string('award_g8');
+            $table->string('award_g9');
+            $table->string('award_g10');
+            $table->string('award_g11');
+            $table->string('award_g12');
+            $table->string('activity_competition_g7');
+            $table->string('activity_competition_g8');
+            $table->string('activity_competition_g9');
+            $table->string('activity_competition_g10');
+            $table->string('activity_competition_g11');
+            $table->string('activity_competition_g12');
+            $table->string('classification_g7');
+            $table->string('classification_g8');
+            $table->string('classification_g9');
+            $table->string('classification_g10');
+            $table->string('classification_g11');
+            $table->string('classification_g12');
+            //participation in seminars/conferences/contest/competitions
+            $table->string('title_g7');
+            $table->string('title_g8');
+            $table->string('title_g9');
+            $table->string('title_g10');
+            $table->string('title_g11');
+            $table->string('title_g12');
+            $table->string('sponsor_g7');
+            $table->string('sponsor_g8');
+            $table->string('sponsor_g9');
+            $table->string('sponsor_g10');
+            $table->string('sponsor_g11');
+            $table->string('sponsor_g12');
+            $table->string('scope_g7');
+            $table->string('scope_g8');
+            $table->string('scope_g9');
+            $table->string('scope_g10');
+            $table->string('scope_g11');
+            $table->string('scope_g12');
+            $table->string('nature_evolvement_b_g7');
+            $table->string('nature_evolvement_b_g8');
+            $table->string('nature_evolvement_b_g9');
+            $table->string('nature_evolvement_b_g10');
+            $table->string('nature_evolvement_b_g11');
+            $table->string('nature_evolvement_b_g12');
             $table->timestamp();
         });
 
-        //creating table for vacational planning
-        // Schema::create('vocational_planning', function (Blueprint $table){
-        //     $table->foreignId('');
-        // });
+        //creating table for vocational planning
+        Schema::create('vocational_plannings', function (Blueprint $table){
+            $table->foreignId('personal_info_id');
+            $table->longText('1st');
+            $table->longText('2nd');
+        });
 
         //creating table for psychological test profile
         Schema::create('psychological_test_profiles', function (Blueprint $table){
-            
-        });
+            $table->foreignId('personal_info_id');
+            //mental
+            $table->longText('culture fair');
+            $table->longText('olmat');
+            //aptitude
+            $table->string('raw_score_a');
+            $table->string('percentile_a');
+            $table->string('description_a');
+            //study habits and attitudes
+            $table->string('raw_score_b');
+            $table->string('percentile_b');
+            $table->string('description_b');
+            //problem checklist
+            $table->string('grade7');
+            $table->string('grade8');
+            $table->string('grade9');
+            $table->string('grade10');
+            $table->string('grade11');
+            $table->string('grade12');
+            //personality
+            $table->integer('reserve');
+            $table->integer('less_inteligent');
+            $table->integer('affected by feelings');
+            $table->integer('phlegmatic');
+            $table->integer('obedient');
+            $table->integer('discregards_rules');
+            $table->integer('shy');
+            $table->integer('tough_minded');
+            $table->integer('sober');
+            $table->integer('vigorous');
+            $table->integer('self_assured');
+            $table->integer('group_dependent');
+            $table->integer('casual');
+            $table->integer('relaxed');
 
+        });
     }
 
     /**
@@ -195,8 +293,13 @@ return new class extends Migration
     {
         Schema::dropIfExists('personal_infos');
         Schema::dropIfExists('family_bgs');
-        Schema::dropIfExists('siblings');
         Schema::dropIfExists('health_physical_devs');
+        Schema::dropIfExists('residences');
         Schema::drapIfExists('social_profiles');
+        Schema::dropIfExists('educational_bg_as');
+        Schema::dropIfExists('students_self_assessments');
+        Schema::dropIfExists('extra_curricular_acts');
+        Schema::dropIfExists('vocational_plannings');
+        Schema::dropIfExists('psychological_test_profiless');
     }
 };
