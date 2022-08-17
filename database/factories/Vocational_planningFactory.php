81c70e4998aecafecf5fef42324624aa0f050f10
jemuel <medzjem@gmail.com>
2022-08-17 15:37:31 +0800

    updated database and factories

---
+++ database/factories/Vocational_planningFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Vocational_planning>
+ */
+class Vocational_planningFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
