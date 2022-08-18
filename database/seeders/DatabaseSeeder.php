f6beda647dc193e33fd123fc18a6ddfa43ce7d22
princelloyd <princelloyd.cagampang@gmail.com>
2022-08-18 15:05:08 +0800

    updated seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,10 +3,17 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Sibling;
+
+use App\Models\Educational_bg;
+use App\Models\extra_curricular_act;
 use App\Models\Health_physical_dev;
 use App\Models\family_bg;
 use App\Models\personal_info;
+use App\Models\Psychological_test_profile;
+use App\Models\Residence;
+use App\Models\Social_profile;
+use App\Models\Students_self_assessment;
+use App\Models\Vocational_planning;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -27,13 +34,43 @@ class DatabaseSeeder extends Seeder
           'personal_info_id' => $personal_info->id
           ]);
 
-          Sibling::factory(2)->create([
-            'personal_info_id' => $personal_info->id
-          ]);
-
           Health_physical_dev::factory()->create([
             'personal_info_id' => $personal_info->id
           ]);
+
+          Residence::factory()->create([
+            'personal_info_id' => $personal_info->id
+          ]);
+
+          Educational_bg::factory()->create([
+            'personal_info_id' => $personal_info->id
+          ]);
+
+          Social_profile::factory()->create([
+            'personal_info_id' => $personal_info->id
+          ]);
+          
+          Students_self_assessment::factory()->create([
+            'personal_info_id' => $personal_info->id
+          ]);
+
+          extra_curricular_act::factory()->create([
+            'personal_info_id' => $personal_info->id
+          ]);
+
+          Vocational_planning::factory()->create([
+            'personal_info_id' => $personal_info->id
+          ]);
+
+          Psychological_test_profile::factory()->create([
+            'personal_info_id' => $personal_info->id
+          ]);
+
+
+
+
+
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
