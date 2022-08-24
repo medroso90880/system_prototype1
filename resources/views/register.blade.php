784299c4bfa6364aca0e4d16329e68fdfc02acd2
jasper <jasperdaquila@gmail.com>
2022-08-24 13:19:57 +0800

    fixed register and dashboard

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,53 +1,30 @@
 <x-layout>
 
-    <x-card class="p-10 rounded max-w-2xl mx-auto mt-24">
 
-        <x-personal-information>
-        </x-personal-information>
-
-    </x-card>
     <x-card class="p-10 rounded max-w-2xl mx-auto mt-24">
-        <x-family-background>
-        </x-family-background>
+        <x-input_cards.family-background/>
     </x-card>
 
     <x-card class="p-10 rounded max-w-2xl mx-auto mt-24">
-        <x-HealthPhysicalDev>
-        </x-HealthPhysicalDev>
+        <x-input_cards.health/>
     </x-card>
     <x-card class="p-10 rounded max-w-2xl mx-auto mt-24">
-        <x-residence-info>
-        </x-residence-info>
+        <x-input_cards.residence-info/>
     </x-card>
 
     <x-card class="p-10 rounded max-w-2xl mx-auto mt-24">
-        <x-educ-background>
-        </x-educ-background>
+        <x-input_cards.education-back/>
     </x-card>
 
     <x-card class="p-10 rounded max-w-2xl mx-auto mt-24">
-        <x-SocialProfile>
-        </x-SocialProfile>
+        <x-input_cards.social-profile/>
     </x-card>
 
     <x-card class="p-10 rounded max-w-2xl mx-auto mt-24">
-        <x-student-self-assessment>
-        </x-student-self-assessment>
-    </x-card>
-    <x-card class="p-10 rounded max-w-6xl mx-auto mt-24">
-        <x-extra-curricular>
-        </x-extra-curricular>
+        <x-input_cards.student-self-assessment/>
     </x-card>
 
-    <x-card class="p-10 rounded max-w-2xl mx-auto mt-24">
-        <x-VocationalPlanning>
-        </x-VocationalPlanning>
-    </x-card>
 
-    <x-card class="p-10 rounded max-w-5xl mx-auto mt-24">
-        <x-psychological-test>
-        </x-psychological-test>
-    </x-card>
     <div class="mb-6 flex justify-center p-10">
         <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
             Submit
